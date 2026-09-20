d3db49aeafc3ab3637661e07e8317b3476b5054d
hnzltrq <hanzla.tariq@zohomail.com>
2026-09-20 14:14:30 +0500

    Fixed exit issue

--- src/main.c
+++ src/main.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include "linenoise.h"
-#include <sys/wait.h>
-#include <unistd.h>
 
 int main(void)
 {
     char * command;
 
-    while ((command = linenoise("What be ye commmand> ")) !=NULL)
+    while ((command = linenoise("What be ye commmand> ")) !=NULL) // loops runs continosuly reading and executing commands
     {
 
         // printf("input: %s\n", command); // Meant to be used for debugging
 
-        if (command[0] == '\0')
+        if (strcmp (command, "exit")==0) // exits shell
         {
             free (command);
             break;
         }
+
+        
         
     }
 }
